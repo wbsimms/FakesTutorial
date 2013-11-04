07c15d8a3346774039499c60bb5c8786738ec225
William Barrett Simms <wbsimms@gmail.com>
2013-11-04 06:34:33 -0500

    Initial Revision

---
+++ FakesTutorialLib/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakesTutorialLib
+{
+    public class Person
+    {
+        private IAddressLookup addressLookup;
+        public Address HomeAddress { get; set; } 
+
+        public Person(int id, IAddressLookup addressLookup)
+        {
+            this.addressLookup = addressLookup;
+            this.HomeAddress = addressLookup.GetAddress(id);
+        }
+    }
+}
---
+++ FakesTutorialTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FakesTutorialLib;
+using FakesTutorialLib.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FakesTutorialTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IAddressLookup mock = new FakesTutorialLib.Fakes.StubIAddressLookup()
+            {
+                GetAddressInt32 = (id) => {return new Address(){Line1 = "blah",Line2= "blah2"};}
+            };
+
+
+            using (ShimsContext.Create())
+            {
+                ShimAddress.AllInstances.Line1Get = address => { return "shimmed"; };
+                Person p = new Person(2, mock);
+                Assert.IsNotNull(p.HomeAddress.Line1);
+            }
+        }
+
+        [TestMethod]
+        public void ShimOnceTest()
+        {
+            using (ShimsContext.Create())
+            {
+                Address address = new ShimAddress() {Line1Get = () => { return "shimmed"; }};
+                Address addr = new ShimAddress().Instance;
+                Assert.IsNull(addr.Line1);
+            }
+        }
+    }
+}
